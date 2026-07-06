b1f59fd440bc01e48e65a4682842ee202755208b
Scope1-guy <the.guyyy1203@gmail.com>
2026-07-06 20:32:01 +0100

    scroll effect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { AuthProvider } from './context/AuthContext';
-import { CartProvider } from './context/CartContext';
-import { WishlistProvider } from './context/WishlistContext';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
+import { WishlistProvider } from "./context/WishlistContext";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
-import Home from './pages/Home/Home';
-import Shop from './pages/Shop/Shop';
-import ProductDetails from './pages/ProductDetails/ProductDetails';
-import Categories from './pages/Categories/Categories';
-import About from './pages/About/About';
-import Contact from './pages/Contact/Contact';
-import Cart from './pages/Cart/Cart';
-import Wishlist from './pages/Wishlist/Wishlist';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import Profile from './pages/Profile/Profile';
-import NotFound from './pages/NotFound/NotFound';
+import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Categories from "./pages/Categories/Categories";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Cart from "./pages/Cart/Cart";
+import Wishlist from "./pages/Wishlist/Wishlist";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Profile from "./pages/Profile/Profile";
+import NotFound from "./pages/NotFound/NotFound";
 
 // Provider order matters here only in that WishlistProvider calls useCart()
 // internally (to support "move to cart"), so CartProvider must wrap it.
@@ -28,6 +29,7 @@ import NotFound from './pages/NotFound/NotFound';
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <AuthProvider>
         <CartProvider>
           <WishlistProvider>
